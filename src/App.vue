823b967d794fa296573ccf51d3021fb16efd9185
Tamila <95583050+TamilaMukh@users.noreply.github.com>
2022-04-19 17:55:56 +0600

    master

--- src/App.vue
+++ src/App.vue
@@ -10,19 +10,21 @@ export default {
   data() {
     return {
       message: "hello",
-      a: [1, 2, 3, 4, 5, 6, 7, 8, 9, 15],
-      b: []
+      a: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      b: [],
+      c: null
     }
   },
    mounted() {
     console.log(this.a);
     for (let i = 0; i <= this.a.length; i++) {
       if(this.a[i] % 2 === 0) {
-        this.b.push(this.a[i]) 
-      } else {
-        console.log('no')
+        this.b.push(this.a[i])
       }
-    console.log(this.a[i])
+      console.log(this.a[i]);
+    }
+    for (let x = 0; x <= this.b.length; x++) {
+      this.c = this.b[x] 
     }
   },
   
